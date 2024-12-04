0d1acc670127da39014dc2ad7d0e8c7b17d0452a
h23pison <h23pison@du.se>
2024-12-04 09:14:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const checkbox = document.getElementById("divStyle");
 // Hämtar alla textfält, lagrar i en variabel
 const inputFields = document.querySelectorAll(".textfield");
 
-<<<<<<< HEAD
-// Hämtar knappen och lagrar i en variabel
+// Hämtar knappen, lagrar i variabel
 const button = document.getElementById("removeDiv");
 
-// Hämtar "emptyDiv" och lagrar i en variabel
-const emptydiv = document.getElementsByClassName("emptyDiv");
+// Hämtar div-elementet, lagrar i variabel
+const emptyDiv = document.getElementsByClassName("emptyDiv");
+
 
 function handleInput(e) {
     const targetName = e.target.name;
@@ -31,10 +31,3 @@ button.addEventListener('click', handleClick);
 checkbox.addEventListener('change');
 
 inputFields.addEventListener('');
-=======
-// Hämtar knappen, lagrar i variabel
-const button = document.getElementById("removeDiv");
-
-// Hämtar div-elementet, lagrar i variabel
-const emptyDiv = document.getElementsByClassName("emptyDiv");
->>>>>>> d6129d416e21eb70bbe135e18faac5fae3c6336f
