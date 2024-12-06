9d28b33a44ed772eada931cc22ab48389888b840
Jonathan Jansson <h23jjans@du.se>
2024-12-06 08:57:47 +0100

    Tagit bort klass och id på form då det inte används, lagt till kommentarer

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Hämtar checkbox, lagrar i variabel
+// Hämtar checkbox, lagrar i variabel:
 const checkbox = document.getElementById("divStyle");
 
-// Hämtar alla textfält, lagrar i en variabel
+// Hämtar alla textfält (lagras i en NodeList), lagrar NodeListan i en variabel:
 const inputFields = document.querySelectorAll(".textfield");
 
-// Hämtar knappen, lagrar i variabel
+// Hämtar knappen, lagrar i variabel:
 const button = document.getElementById("removeDiv");
 
-// Hämtar div-elementet, lagrar i variabel
+// Hämtar div-elementet, lagrar i variabel:
 const emptyDiv = document.querySelector(".emptyDiv");
 
+// Fördefinierad funktion som kommer att användas som eventlyssnare till inputfälten:
 function handleInput(e) {
     const targetName = e.target.name;
     const targetField = e.target.value;
@@ -20,17 +21,20 @@ function handleInput(e) {
     }
 }
 
+// Eventlyssnare till knappen, med anonym funktion:
 button.addEventListener('click', (e) => {
     e.preventDefault();
     emptyDiv.remove();
     console.log('Någon klickade på "Ta bort" knappen! Parameter, ', e);
 });
 
+// Eventlyssnare till checkbox, med anonym funktion:
 checkbox.addEventListener('change', (e) => {
-    const colorField = document.getElementById('color');
+    const colorField = inputFields[0];
     emptyDiv.style.backgroundColor = colorField.value;
 });
 
+// Eventlyssnare till textfälten, med tidigare skapad funktion kopplad till:
 inputFields.forEach(field => {
     field.addEventListener("input", handleInput)
     field.addEventListener("blur", handleInput)
